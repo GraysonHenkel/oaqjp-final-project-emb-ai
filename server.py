ca0a5d1e42b3aee93ff67a2ccf417c5e4010329f
GraysonHenkel <graysonhenkel@hotmail.com>
2025-09-05 22:59:37 -0400

    Updated server.py to follow coding standards

--- server.py
+++ server.py
@@ -1,11 +1,18 @@
+''' Executing this function initiates the application of emotion
+    detection to be executed over the Flask channel and deployed on
+    localhost:5000.
+'''
 from flask import Flask, render_template, request
 from EmotionDetection.emotion_detection import emotion_detector
-import requests  
-
 app = Flask("Emotion Detector")
 
 @app.route("/emotionDetector")
 def emot_detector():
+    ''' This code receives the text from the HTML interface and 
+    runs emotion detection over it using emotion_detector()
+    function. The output returned shows the emotions, their score,
+    and the dominant emotion for the provided text.
+    '''
     # Retrieve the text to analyze from the request arguments
     text_to_analyze = request.args.get('textToAnalyze')
     # Pass the text to the emotion_detector function and store the response
@@ -35,6 +42,5 @@ def render_index_page():
     return render_template('index.html')
 
 if __name__ == "__main__":
-    ''' This functions executes the flask app and deploys it on localhost:5000
-    '''
+    # This functions executes the flask app and deploys it on localhost:5000
     app.run(host="0.0.0.0", port=5000)
